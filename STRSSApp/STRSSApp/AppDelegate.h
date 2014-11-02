25ee894b19886a6853c5873a6d3153276ddaac11
MerryDev <merrykimu@gamail.com>
2014-11-03 00:07:28 +0900

    add project file

---
+++ STRSSApp/STRSSApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STRSSApp
+//
+//  Created by MerryMacMini on 2014. 11. 3..
+//  Copyright (c) 2014년 devst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ STRSSApp/STRSSApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STRSSApp
+//
+//  Created by MerryMacMini on 2014. 11. 3..
+//  Copyright (c) 2014년 devst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
